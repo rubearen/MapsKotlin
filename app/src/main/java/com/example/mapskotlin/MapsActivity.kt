6e07e3f93a997cde204a513fe57973cb8b2dde68
rubearen <rubearen@gmail.com>
2019-12-16 19:53:01 +0100

    funcionando

--- app/src/main/java/com/example/mapskotlin/MapsActivity.kt
+++ app/src/main/java/com/example/mapskotlin/MapsActivity.kt
@@ -4,7 +4,6 @@ package com.example.mapskotlin
 import android.content.pm.PackageManager
 import android.location.Location
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import com.google.android.gms.location.FusedLocationProviderClient
@@ -89,7 +88,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarker
 
 
             )
-            
+
             return
         }
     }
